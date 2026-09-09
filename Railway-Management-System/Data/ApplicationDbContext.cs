bfb3176c0e313e8d301134c609b101a549b7511b
aceGrujevski <aleksandargrujevski125@gmail.com>
2026-09-10 01:03:21 +0200

    Initial

---
+++ Railway-Management-System/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Railway_Management_System.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+{
+}
